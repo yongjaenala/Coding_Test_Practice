4fc027a6b0b2b2eaaf3522cb2bc7a42de60f2327
ryunada <rst3047@gmail.com>
2022-10-19 23:04:26 +0900

    10.19

--- Ryu/Lv0/옹알이.py
+++ Ryu/Lv0/옹알이.py
@@ -1,10 +1,15 @@
 def solution(babbling):
     shy = ["aya", "ye", "woo", "ma"]
     answer = 0
-    for i in len(babbling):
-        for j in len(shy):
-            if shy[j] in babbling[i]:
-                babbling[i]
-                answer ++
-
+    for i in babbling:
+        for j in shy:
+            if j*2 in i: # 같은 발음을 하는 것을 어려워 함
+                continue
+            else:
+               i = i.replace(j, "")
+        if len(i.strip()) == 0:
+                answer += 1
     return answer
+
+babbling = ["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]
+print(solution(babbling))
