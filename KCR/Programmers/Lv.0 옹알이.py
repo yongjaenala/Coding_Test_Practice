22fd6f0dda715ce21ed06116f758e653cc6fd437
Kim-Chae-Rin <rin1222k@naver.com>
2022-10-21 00:53:01 +0900

    코딩테스트 입문 > 옹알이

--- KCR/Programmers/Lv.0 옹알이.py
+++ KCR/Programmers/Lv.0 옹알이.py
@@ -1,9 +1,9 @@
 def solution(babbling):
     cnt = 0
     for b in babbling :
-        for c in ["aya", "ye", "woo", "ma"] :
-            if c*2 not in b :
-                b = b.replace(c,'')
+        for i in ["aya", "ye", "woo", "ma"] :
+            if i*2 not in b :
+                b = b.replace(i,'')
         if len(b) == 0 :
             cnt += 1
     return cnt
