f9e62ffd6790182ead77b383a27ee2e01a62450c
Junmo Ryu <114718386+junmosdata@users.noreply.github.com>
2022-10-21 14:27:32 +0900

    Add files via upload

---
+++ Junmo/Programmers/Lv. 0/두 수의 몫.py
@@ -0,0 +1,10 @@
+# 두 수의 몫
+
+def solution(num1, num2):
+    answer = 0
+
+    answer = num1 // num2
+
+    return answer
+
+print(solution(5, 2))
---
+++ Junmo/Programmers/Lv. 0/편지.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120898
+# 편지
+
+def solution(message):
+    answer = 0
+
+    answer = 2 * len(message)
+
+    return answer
