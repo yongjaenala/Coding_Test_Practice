e70ca11e789444dff761bd0eff11d4b4dbdadf48
ryunada <rst3047@gmail.com>
2022-10-21 18:48:51 +0900

    10.21(금)

--- Ryu/Lv0/평행.py
+++ Ryu/Lv0/평행.py
@@ -3,12 +3,13 @@ def solution(dots):
     slop = []
     for i in range(0,len(dots)-1):
         for j in range(i+1,len(dots)):
+            if(dots[j][0]-dots[i][0]) == 0:
+                continue
             slop.append((dots[j][1]-dots[i][1])/(dots[j][0]-dots[i][0]))
 
-    # for i in range(0,len(slop)-1):
-    #     for j in range(i+1,len(slop)):
-    #         if (slop[i] == slop[j]):
-    #             answer += 1
+    for i in range(0,len(dots)-1):
+        for j in range(i+1,len(dots)):
+            dots[j]
     if (len(slop) == len(set(slop))):
         return 0
     return 1
