e18626b7d2d196f9892d357551ecdbc2f70ee861
Kim-Chae-Rin <rin1222k@naver.com>
2022-10-22 15:23:30 +0900

    코딩테스트 입문 > 연속된 수의 합

--- KCR/Programmers/Lv.0/Lv.0 연속된 수의 합.py
+++ KCR/Programmers/Lv.0/Lv.0 연속된 수의 합.py
@@ -1,3 +1,6 @@
-def solution(num, total):
+## 4+5+6 = 4+(4+1)+(4+2) = 4*3 + (1+2) = 15
+## 4+5+6+7 = 4+(4+1)+(4+2)+(4+3) = 4*4 + (1+2+3) = 22
+# n = 4
+def solution(num, total):  # 연속된 수의 개수, 총합
     n = (total-sum(range(1,num)))//num
     return list(range(n,n+num))
