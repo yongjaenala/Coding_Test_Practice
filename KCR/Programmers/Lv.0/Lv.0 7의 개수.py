5cff271cbb5d3aa41909470dbfc238af375f9a25
Kim-Chae-Rin <rin1222k@naver.com>
2022-10-22 14:47:44 +0900

    코딩테스트 입문 > 7의 개수

--- KCR/Programmers/Lv.0/Lv.0 7의 개수.py
+++ KCR/Programmers/Lv.0/Lv.0 7의 개수.py
@@ -2,5 +2,5 @@ def solution(array):
     answer = 0
     for i in array:
         if '7' in str(i) :
-            answer += str(i).count('7')
+            answer += str(i).count('7') # 7 -> 1 ; 77 -> 2
     return answer
