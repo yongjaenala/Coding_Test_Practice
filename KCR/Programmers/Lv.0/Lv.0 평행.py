d6a0435ef560e144d9d948c601990ee7a35d5a75
Kim-Chae-Rin <rin1222k@naver.com>
2022-10-22 14:42:10 +0900

    코딩테스트 입문 > 평행

--- KCR/Programmers/Lv.0/Lv.0 평행.py
+++ KCR/Programmers/Lv.0/Lv.0 평행.py
@@ -5,7 +5,7 @@ def solution(dots):
             case.append([i, j])
     gradi = []
     for c in case :
-        if (dots[c[0]][0]-dots[c[1]][0]) != 0 :
+        if (dots[c[0]][0]-dots[c[1]][0]) != 0 :  # 분모가 0이 아닌 경우
             gradient =(dots[c[0]][1]-dots[c[1]][1])/(dots[c[0]][0]-dots[c[1]][0])
             gradi.append(gradient)
 
