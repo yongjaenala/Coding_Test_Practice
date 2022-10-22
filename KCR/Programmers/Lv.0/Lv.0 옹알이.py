b0c40e894604fc109fbbc4353bee6dfa2a916d7b
Kim-Chae-Rin <rin1222k@naver.com>
2022-10-22 15:24:49 +0900

    코딩테스트 입문 > 옹알이

--- KCR/Programmers/Lv.0/Lv.0 옹알이.py
+++ KCR/Programmers/Lv.0/Lv.0 옹알이.py
@@ -2,8 +2,8 @@ def solution(babbling):
     cnt = 0
     for b in babbling :
         for i in ["aya", "ye", "woo", "ma"] :
-            if i*2 not in b :
+            if i*2 not in b :  # 연속하는지 확인
                 b = b.replace(i,'')
-        if len(b) == 0 :
+        if len(b) == 0 : # 문자열이 끝났는지 확인
             cnt += 1
     return cnt
