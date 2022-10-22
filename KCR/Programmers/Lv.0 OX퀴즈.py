70fd71452adb3b6a2537280b496c74709967c081
Kim-Chae-Rin <rin1222k@naver.com>
2022-10-22 14:21:47 +0900

    코딩테스트 입문 > OX퀴즈

--- KCR/Programmers/Lv.0 OX퀴즈.py
+++ KCR/Programmers/Lv.0 OX퀴즈.py
@@ -1,9 +1,11 @@
-quiz = ["3 - 4 = -3", "5 + 6 = 11"]	# ["X", "O"]
-quiz = ["19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"]	# ["O", "O", "X", "O"]
-
 def solution(quiz):
+    answer = []
     for q in quiz :
         quiz = q.split('=')
-        answer = quiz[1]
+        result = quiz[1]
         problem = quiz[0]
-        if 
+        if eval(problem) == int(result) :
+            answer.append('O')
+        else :
+            answer.append('X')
+    return answer
