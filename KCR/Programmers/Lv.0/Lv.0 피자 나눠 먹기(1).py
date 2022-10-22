b908609c55318cd9072db09a40553904d324e6c5
Kim-Chae-Rin <rin1222k@naver.com>
2022-10-22 15:02:35 +0900

    코딩테스트 입문 > 피자 나눠 먹기(1)

--- KCR/Programmers/Lv.0/Lv.0 피자 나눠 먹기(1).py
+++ KCR/Programmers/Lv.0/Lv.0 피자 나눠 먹기(1).py
@@ -1,3 +1,11 @@
+# 첫 번째 풀이
 import numpy as np
 def solution(n):
     return np.ceil(n/7)
+
+# 두 번째 풀이 : 함수X
+def solution(n):
+    if n%7 != 0 :
+        return (n//7)+1
+    else :
+        return n//7
