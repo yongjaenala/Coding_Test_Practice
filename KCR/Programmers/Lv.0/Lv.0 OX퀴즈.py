11d0ba78565910c3f47ed1ea6bab13bfc5abe5f7
Kim-Chae-Rin <rin1222k@naver.com>
2022-10-22 14:49:21 +0900

    코딩테스트 입문 > OX퀴즈

--- KCR/Programmers/Lv.0/Lv.0 OX퀴즈.py
+++ KCR/Programmers/Lv.0/Lv.0 OX퀴즈.py
@@ -4,7 +4,7 @@ def solution(quiz):
         quiz = q.split('=')
         result = quiz[1]
         problem = quiz[0]
-        if eval(problem) == int(result) :
+        if eval(problem) == int(result) : # eval : 문자열 계산식을 계산해주는 함수
             answer.append('O')
         else :
             answer.append('X')
