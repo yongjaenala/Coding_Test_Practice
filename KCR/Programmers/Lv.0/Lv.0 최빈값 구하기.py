c32713e7dbbb87bbc15ebcf75963a6a6b3f3b5bf
Kim-Chae-Rin <rin1222k@naver.com>
2022-10-22 14:58:34 +0900

    코딩테스트 입문 > 최빈값 구하기

--- KCR/Programmers/Lv.0/Lv.0 최빈값 구하기.py
+++ KCR/Programmers/Lv.0/Lv.0 최빈값 구하기.py
@@ -1,15 +1,13 @@
 import numpy as np
 def solution(array):
-    if len(array) == 1:
-        return array[0]
 
-    a = [0]*(max(array)+1)  
+    a = [0]*(max(array)+1)  # 인덱스 = 숫자 ; 값 = count
 
     for i in array :
         a[i] += 1
-    mx = int(np.argmax(a))
-    mx_v = max(a)
-    a[mx] = 0
-    if mx_v == max(a) :
+    mx = int(np.argmax(a))  # argmax : 배열에서 최댓값의 인덱스를 반환
+    mx_v = max(a)  # 최빈값의 개수
+    a[mx] = 0  # 최빈값 개수 초기화
+    if mx_v == max(a) :  # 두 번째로 많은 수의 개수와 최빈값 개수 비교
         return -1
     return mx
