982ede5bf7e74e04a19e5737645ff0d2aba24d55
bbhsrh <gaon726@naver.com>
2022-10-24 21:55:31 +0900

    Day3_gaoni_complete

--- Gaoni/programmers/Lv0/011최빈값구하기.py
+++ Gaoni/programmers/Lv0/011최빈값구하기.py
@@ -1,27 +1,49 @@
+# import numpy as np
+#
+#
+# def solution(array):
+#     answer = 0
+#
+#     wonso = [0] * 1001
+#
+#     for i in array:
+#         wonso[i] += 1
+#
+#     f_max_i = int(np.argmax(wonso))
+#     s_max_i = int(np.argmax(wonso[f_max_i:])) + f_max_i + 1
+#
+#     f_max_v = wonso[f_max_i]
+#     s_max_v = wonso[s_max_i]
+#
+#     if f_max_v == s_max_v:
+#         answer = -1
+#     else:
+#         answer = f_max_i
+#
+#     #    print(wonso)
+#     #    print(f_max)
+#     #    print(s_max)
+#
+#     return answer
 import numpy as np
 
 
 def solution(array):
     answer = 0
 
-    wonso = [0] * 1001
-
-    for i in array:
-        wonso[i] += 1
-
-    f_max_i = int(np.argmax(wonso))
-    s_max_i = int(np.argmax(wonso[f_max_i:])) + f_max_i + 1
-
-    f_max_v = wonso[f_max_i]
-    s_max_v = wonso[s_max_i]
-
-    if f_max_v == s_max_v:
-        answer = -1
+    if len(array) == 1:
+        answer = array[0]
     else:
-        answer = f_max_i
+        wonso = [0] * 1001
 
-    #    print(wonso)
-    #    print(f_max)
-    #    print(s_max)
+        for i in array:
+            wonso[i] += 1
+
+        f_max_i = int(np.argmax(wonso))
+
+        if wonso[f_max_i] == wonso[int(np.argmax(wonso[f_max_i + 1:])) + f_max_i + 1]:
+            answer = -1
+        else:
+            answer = f_max_i
 
     return answer
