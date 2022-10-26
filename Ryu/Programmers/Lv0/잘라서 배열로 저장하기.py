1c8a74d6eaab4ec488bdc464a541c5fbff4fa1d8
ryunada <rst3047@gmail.com>
2022-10-26 23:57:13 +0900

    10.26(수)

--- Ryu/Programmers/Lv0/잘라서 배열로 저장하기.py
+++ Ryu/Programmers/Lv0/잘라서 배열로 저장하기.py
@@ -1,5 +1,14 @@
+import math
 def solution(my_str, n):
-    for i in range(0, len(my_str), n):
+    answer = []
+    for i in range(0,math.ceil(len(my_str)/n)): # math.ceil  : 올림
+        answer.append(my_str[:n])
+        my_str = my_str[n:]
+    return answer
 
+def other_solution(my_str,n):
+    return [my_str[i : i + n] for i in range(0, len(my_str),n)]
 
-solution("abc1Addfggg4556b",6)
+print(solution("abc1Addfggg4556b",6))
+print(other_solution("abc1Addfggg4556b",6))
+
