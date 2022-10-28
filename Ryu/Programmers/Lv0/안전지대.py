bee2a0eed2c4c0557964e4318f929e92665ea184
ryunada <rst3047@gmail.com>
2022-10-28 09:06:28 +0900

    10.28(금)

--- Ryu/Programmers/Lv0/안전지대.py
+++ Ryu/Programmers/Lv0/안전지대.py
@@ -4,29 +4,30 @@ def solution(board):
     for i in range(0,len(board)):
         for j in range(0, len(board)):
             try:
-                if (board[i][j]%10) == 1:
-                    board[i - 1][j-1] += 10
+                if (board[i][j] % 10) == 1:
+                    board[i - 1][j - 1] += 10
                     board[i - 1][j] += 10
                     board[i - 1][j + 1] += 10
                     board[i][j - 1] += 10
                     board[i][j] += 10
                     board[i][j + 1] += 10
-                    board[i+1][j - 1] += 10
-                    board[i+1][j] += 10
-                    board[i+1][j + 1] += 10
+                    board[i + 1][j - 1] += 10
+                    board[i + 1][j] += 10
+                    board[i + 1][j + 1] += 10
             except:
                 pass
     for i in range(0,len(board)):
         for j in range(0, len(board)):
             if board[i][j] == 0:
                 answer += 1
-    return answer
+    return answer, board
 
 board1 = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0]]
 board2 = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 1, 1, 0], [0, 0, 0, 0, 0]]
 board3 = [[1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]]
 board4 = [[0,0,0,0,0],[0,0,0,0,1],[0,0,0,0,1],[0,0,0,0,1],[0,0,0,0,1]]
+board5 = [[0, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0], [1, 0, 0, 0, 0]]
 
 
 
-print(solution(board2))
+print(solution(board5))
