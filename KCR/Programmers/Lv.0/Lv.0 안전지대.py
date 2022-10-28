3c0bce25de78091080fe06db8ad56c8ecce15ca5
Kim-Chae-Rin <kcrindata@gmail.com>
2022-10-28 17:44:31 +0900

    이거 수정해야 함

--- KCR/Programmers/Lv.0/Lv.0 안전지대.py
+++ KCR/Programmers/Lv.0/Lv.0 안전지대.py
@@ -3,25 +3,33 @@
 def solution(board):
     if 0 not in sum(board,[]) :
         return 0
-    for i in range(len(board)) :
-        for j in range(len(board)) :
-            if board[i][j] == 1 :
-                if (i != 0) & (board[i-1][j] != 1) :
-                    board[i-1][j] = 2
-                if (i != len(board)-1) & (board[i+1][j] != 1) :
-                    board[i+1][j] = 2
-                if (j != 0) & (board[i][j-1] != 1) :
-                    board[i][j-1] = 2
-                if (j != len(board)-1) & (board[i][j+1] != 1) :
-                    board[i][j+1] = 2
-                if (i != 0) & (j != 0) & (board[i-1][j-1] != 1) :
-                    board[i-1][j-1] = 2
-                if (i != 0) & (j != len(board)-1) & (board[i-1][j+1] != 1) :
-                    board[i-1][j+1] = 2
-                if (i != len(board)-1) & (j != 0) & (board[i+1][j-1] != 1) :
-                    board[i+1][j-1] = 2
-                if (i != len(board)-1) & (j != len(board)-1) & (board[i+1][j+1] != 1) :
-                    board[i+1][j+1] = 2
+    for i in range(0,len(board)) :
+        for j in range(0,len(board)) :
+            try:
+                if board[i][j] == 1 :
+                    if (i != 0) & (board[i-1][j] != 1) :
+                        board[i-1][j] = 2
+                    if (i != len(board)-1) & (board[i+1][j] != 1) :
+                        board[i+1][j] = 2
+                    if (j != 0) & (board[i][j-1] != 1) :
+                        board[i][j-1] = 2
+                    if (j != len(board)-1) & (board[i][j+1] != 1) :
+                        board[i][j+1] = 2
+                    if (i != 0) & (j != 0) & (board[i-1][j-1] != 1) :
+                        board[i-1][j-1] = 2
+                    if (i != 0) & (j != len(board)-1) & (board[i-1][j+1] != 1) :
+                        board[i-1][j+1] = 2
+                    if (i != len(board)-1) & (j != 0) & (board[i+1][j-1] != 1) :
+                        board[i+1][j-1] = 2
+                    if (i != len(board)-1) & (j != len(board)-1) & (board[i+1][j+1] != 1) :
+                        board[i+1][j+1] = 2
+            except :
+                pass
 
     result = sum(board,[])
-    return result.count(0)
+    return board
+
+board5 = [[0, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0], [1, 0, 0, 0, 0]]
+print(solution(board5))
+for i in board5 :
+    print(i)
---
+++ KCR/Programmers/Lv.0/Lv.0 저주의 숫자 3.py
@@ -0,0 +1,14 @@
+def solution(n):
+    contain = 0
+    multiple = 0
+    for i in range(1, n+1) :
+        if i%3 == 0 :
+            multiple += 1
+        na = i%10
+        # if na%3 == 0 :
+        #     contain += 1
+    return n+contain+multipledfsd
+
+n = 15	#25
+# n = 40	#76/cxvdfsdfasgasgfsdakfhladkfhk nx   jKJkslhfksdhjfkahdfkajfhksadfxz
+print(solution(n))
