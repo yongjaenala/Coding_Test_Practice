7b89698ebe330def876dec09188baae3c7b2a9e8
Kim-Chae-Rin <rin1222k@naver.com>
2022-10-29 14:36:28 +0900

    코딩테스트 입문 > 숨어있는 숫자의 덧셈 (1)

---
+++ KCR/Programmers/Lv.0/Lv.0 숨어있는 숫자의 덧셈 (1).py
@@ -0,0 +1,7 @@
+def solution(my_string):
+    num = [str(n) for n in range(10)]
+    answer = []
+    for i in my_string :
+        if i in num :
+            answer.append(int(i))
+    return sum(answer)
