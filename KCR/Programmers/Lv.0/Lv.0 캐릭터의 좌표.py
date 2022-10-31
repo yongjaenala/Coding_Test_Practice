eebb1d20a81509d85f0ae355b27608f62c7bf2c4
Kim-Chae-Rin <rin1222k@naver.com>
2022-10-31 19:42:46 +0900

    코딩테스트 입문 > 캐릭터의 좌표

--- KCR/Programmers/Lv.0/Lv.0 캐릭터의 좌표.py
+++ KCR/Programmers/Lv.0/Lv.0 캐릭터의 좌표.py
@@ -1,25 +1,20 @@
 def solution(keyinput, board):
-    loc = [0]*4 #[right, left, up, down]
+    answer = [0, 0]
     for i in keyinput :
         if i == 'right' :
-            loc[0] += 1
+            answer[0] += 1
+            if answer[0] > board[0]//2 :
+                answer[0] = board[0]//2
         elif i == 'left' :
-            loc[1] += 1
+            answer[0] -= 1
+            if answer[0] < board[0]//2*(-1) :
+                answer[0] = board[0]//2*(-1)
         elif i == 'up' :
-            loc[2] += 1
+            answer[1] += 1
+            if answer[1] > board[1]//2 :
+                answer[1] = board[1]//2
         else :
-            loc[3] += 1
-    answer = [0+loc[0]-loc[1], 0+loc[2]-loc[3]]
-    if answer[0] > board[0] :
-        answer[0] = board[0]//2
-    elif answer[0] < board[0]*(-1) :
-        answer[0] = board[0]//2*(-1)
-    elif answer[1] > board[1]:
-        answer[1] = board[1] // 2
-    elif answer[1] < board[1] * (-1):
-        answer[1] = board[1] // 2 * (-1)
+            answer[1] -= 1
+            if answer[1] < board[1] // 2 * (-1):
+                answer[1] = board[1] // 2 * (-1)
     return answer
-
-keyinput = ["left", "right", "up", "right", "right"]	; board = [11, 11]	#[2, 1]
-keyinput = ["down", "down", "down", "down", "down"]	; board = [7, 9]	#[0, -4]
-print(solution(keyinput,board))
