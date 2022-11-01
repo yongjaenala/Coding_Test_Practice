5feb433393330d9bbf566eb77838673aa28344d9
junmosdata <junmosdata@gmail.com>
2022-11-02 08:52:00 +0900

    11.01, 08:50

---
+++ Junmo/Programmers/Lv. 0/컨트롤 제트.py
@@ -0,0 +1,19 @@
+# 미완
+# https://school.programmers.co.kr/learn/courses/30/lessons/120853
+# 컨트롤 제트
+
+def solution(s):
+    answer = 0
+
+    sList = s.split()
+    for i in range(len(sList) - 1):
+        if sList[i + 1] == "Z":
+            sList[i] = "0"
+    for s in sList:
+        try:
+            answer += int(s)
+        except:
+            pass
+
+    return answer
+
