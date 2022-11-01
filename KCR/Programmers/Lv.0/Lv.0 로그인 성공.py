2b9e0c4b90cd2436f27a2fadb32af95d650f3997
Kim-Chae-Rin <kcrindata@gmail.com>
2022-11-01 09:25:08 +0900

    코딩테스트 입문 > 로그인 성공

--- KCR/Programmers/Lv.0/Lv.0 로그인 성공.py
+++ KCR/Programmers/Lv.0/Lv.0 로그인 성공.py
@@ -1,16 +1,12 @@
 def solution(id_pw, db):
-    w_id = 0
-    w_pw = 0
+    id = []
     for i in db :
-        if id_pw[0] != i[0] :
-            w_id += 1
-            continue
-        if w_id == len(db) :
-            return 'fail'
-        if (id_pw[0] == i[0]) & (id_pw[0])
-
-
-id_pw = ["meosseugi", "1234"]	; db = [["rardss", "123"], ["yyoom", "1234"], ["meosseugi", "1234"]]	#"login"
-id_pw = ["programmer01", "15789"]	; db=[["programmer02", "111111"], ["programmer00", "134"], ["programmer01", "1145"]]	#"wrong pw"
-id_pw = ["rabbit04", "98761"]	; db=[["jaja11", "98761"], ["krong0313", "29440"], ["rabbit00", "111333"]]	#"fail"
-print(solution(id_pw,db))
+        id.append(i[0])
+    if id_pw in db :
+        return "login"
+    else :
+        if id_pw[0] not in id :
+            return "fail"
+        else :
+            if id_pw not in db:
+                return "wrong pw"
