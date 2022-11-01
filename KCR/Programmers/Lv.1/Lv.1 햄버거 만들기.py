1d7e3673807fc04821fb083325c2f92b2e249d3b
Kim-Chae-Rin <kcrindata@gmail.com>
2022-11-02 08:51:40 +0900

    코딩테스트 연습 > 연습문제 > 햄버거 만들기

--- KCR/Programmers/Lv.1/Lv.1 햄버거 만들기.py
+++ KCR/Programmers/Lv.1/Lv.1 햄버거 만들기.py
@@ -1,9 +1,9 @@
-# 첫 번째 풀이 : 시간 초과
 def solution(ingredient):
     line = ''
     cnt = 0
     for i in ingredient :
         line += str(i)
+        # if '1231' in line : -> 시간초과 for문이 한 번 더 돌아가는 거라 O(n^2)
         if line[-4:] == '1231':
             line = line[:-4]
             cnt += 1
