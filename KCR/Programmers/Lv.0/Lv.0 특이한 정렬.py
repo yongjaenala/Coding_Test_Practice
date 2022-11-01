53660482f8bf0be164ddf57df2c5aee1e14db2cd
Kim-Chae-Rin <kcrindata@gmail.com>
2022-11-01 09:37:11 +0900

    코딩테스트 입문 > 특이한 정렬

--- KCR/Programmers/Lv.0/Lv.0 특이한 정렬.py
+++ KCR/Programmers/Lv.0/Lv.0 특이한 정렬.py
@@ -1,6 +1,8 @@
 def solution(numlist, n):
-    numlist.sort()
-    up = []
-    down = []
-numlist = [1, 2, 3, 4, 5, 6]	; n = 4	#[4, 5, 3, 6, 2, 1]
-numlist = [10000,20,36,47,40,6,10,7000]	; n = 30	#[36, 40, 20, 47, 10, 6, 7000, 10000]
+    for i, j in enumerate(numlist) :
+        numlist[i] = (abs(n-j), j)
+    numlist.sort(key = lambda x : (x[0], -x[1]))
+    answer = []
+    for i in numlist :
+        answer.append(i[1])
+    return answer
