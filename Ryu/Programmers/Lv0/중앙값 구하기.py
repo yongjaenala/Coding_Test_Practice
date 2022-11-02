7cbce88fa6438780630b08fbbcf9912041672981
ryunada <rst3047@gmail.com>
2022-11-02 16:03:33 +0900

    11.2(수)

--- Ryu/Programmers/Lv0/중앙값 구하기.py
+++ Ryu/Programmers/Lv0/중앙값 구하기.py
@@ -1,11 +1,16 @@
-# 미해결
+
 def solution(array):
-    if len(array) % 2 == 0:
-        return array[len(array)//2]
-    else:
-        return array[len(array)//2-1]
+    array.sort()
+    return array[len(array)//2]
 
-
+# def solution(array):
+#     array.sort()
+#     if len(array) == 1:
+#         return array[0]
+#     elif len(array) % 2 == 0:
+#         return (array[len(array)//2 - 1]+array[len(array)//2])/2
+#     elif len(array) % 2 == 1:
+#         return array[len(array)//2]
 # array = [1,2,7,10,11]]
 array = [9, -1, 0,1]
 print(len(array)/2)
