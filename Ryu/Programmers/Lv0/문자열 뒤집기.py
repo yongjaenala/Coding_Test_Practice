c43237522668d9c4d54c06fb51e55c7cd9eee3c2
ryunada <rst3047@gmail.com>
2022-11-03 09:13:48 +0900

    11.3(목)

---
+++ Ryu/Programmers/Lv0/문자열 뒤집기.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = ''
+    for i in range(len(my_string)-1, -1, -1):
+        answer += my_string[i]
+    return answer
+
+my_string = 'jaron'
+print(solution(my_string))
