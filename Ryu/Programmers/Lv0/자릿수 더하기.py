9ed63fb5bf7a94e1b4021077a964228dff1eb63d
ryunada <rst3047@gmail.com>
2022-11-06 23:39:04 +0900

    11.6(일)

--- Ryu/Programmers/Lv0/자릿수 더하기.py
+++ Ryu/Programmers/Lv0/자릿수 더하기.py
@@ -1,6 +1,18 @@
+# def solution(n):
+#     answer = 0
+#     for i in range(0,10):
+#         answer += n % 10
+#         if (n//10) == 0:
+#             break
+#         else:
+#             n = n // 10
+#     return answer
+
 def solution(n):
     answer = 0
-    while(1):
+    while n:
         answer += n % 10
-        n/10
+        n = n//10
     return answer
+n = 1234
+print(solution(n))
