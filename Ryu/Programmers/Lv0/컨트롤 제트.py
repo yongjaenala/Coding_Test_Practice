63c53735850ca20fe33fdd1468706f27b0abccbb
SeungHo Ryu <87309905+ryunada@users.noreply.github.com>
2022-11-07 08:38:30 +0900

    11.07(월)

--- Ryu/Programmers/Lv0/컨트롤 제트.py
+++ Ryu/Programmers/Lv0/컨트롤 제트.py
@@ -1,20 +1,29 @@
-# 미해결
-# ctrl+z
-def solution(s):
-    answer = 0
-    s = s.split(' ')
-    ctrl_z = []
-    for i in range(0, len(s)):
-        if (s[i] == 'Z'):
-            if i == 0:      # 첫번째 Z인 경우
-                continue
-            else:
-                answer -= int(ctrl_z[-1])
-                ctrl_z.drop[-1]
-        answer += int(s[i])
-        ctrl_z.append(s[i])
-    return answer
+# ctrl+z(stack으로 풀어야 함)
 
+# def solution(s):
+#     answer = 0
+#     s = s.split(' ')
+#     ctrl_z = []
+#     for i in s:
+#         ctrl_z.append(i)
+#         if (ctrl_z[-1] == 'Z'):
+#             if i == 0:      # 첫번째 Z인 경우
+#                 ctrl_z = 0
+#             else:
+#                 del ctrl_z[-2:]
+#     for j in ctrl_z:
+#         answer += int(j)
+#     return answer
+
+def solution(s):
+    s = s.split(' ')
+    result = []
+    for i in s:
+        if i == 'Z':
+            result.pop()
+        else:
+            result.append(int(i))
+    return sum(result)
 
 
 # s = "1 2 Z 3"
@@ -28,5 +37,7 @@ def solution(s):
 # s = "-1 0 Z 1 -1 0 Z 1 -1 0 Z 1 -1 0 Z 1 -1 0 Z 1 -1 0 Z 1 -1 0 Z 1 -1 0 Z 1 -1 0 Z 1 -1 0 Z 1 -1 0 Z 1 -1 0 Z 1 -1 0 Z 1"
 s = "-100 10 Z 11 12 Z Z"
 
+print(solution(s))
 answer = 0
+
 # print(s.split(' '))
