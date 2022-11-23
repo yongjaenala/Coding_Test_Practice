784ce49ec12552aa89b35655f7133d1ac26e8798
yongjaenala <skwonyj97@naver.com>
2022-11-23 17:48:25 +0900

    11.23(미완성)

---
+++ KYJ/Programmers/Lv 0/22.11.23.py
@@ -0,0 +1,25 @@
+# 어떤 자연수를 제곱했을때 나오는 정수를 제곱수라고 합니다.
+# 정수 n이 매개변수로 주어질때, n이 제곱수라면 1을 아니라면2를 return하도록
+# solution 함수를 완성하시오
+
+
+def solution(n):
+    for i in range(0, n):
+        if i*2 <= n+1:
+            if n != i**2:
+                pass
+            else:
+                answer = 1
+                break
+        else:
+            answer = 2
+
+    return answer
+
+print(solution(144))
+print(solution(976))
+
+print(solution(10000))
+print(solution(5))
+print(solution(4))
+print(solution(1))
